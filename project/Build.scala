61d3409a98a076e506129e37191725db4e00c363
Juan Jose Olivera <juan@genso.com.bo>
2014-07-14 19:06:38 -0400

    responsive top menu

--- project/Build.scala
+++ project/Build.scala
@@ -16,7 +16,10 @@ object LiftProjectBuild extends Build {
         "org.eclipse.jetty" % "jetty-webapp" % Ver.jetty % "container",
         "ch.qos.logback" % "logback-classic" % "1.0.13" % "compile",
         "org.scalatest" %% "scalatest" % "1.9.2" % "test",
-        "com.foursquare" %% "rogue-lift" % "2.2.0"
+        "com.foursquare" %% "rogue-lift" % "2.2.0",
+        "net.liftmodules" %% "openid_2.6" % "1.2",
+        "net.liftmodules" %% "facebook_2.6" % "1.3",
+        "net.liftmodules" %% ("omniauth_"+Ver.lift_edition) % "0.13" % "compile"
       )
     )
 }
